1021b93dd006ed75686c7c6c3cf0053db6315c7d
Venkatesh <pasupuletivenkatesh1900@gmail.com>
2023-03-18 17:15:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses"
 function App() {
-  const expense = [
+  const expenses = [
     {
       title: "Toilet Paper",
       amount: 94.12,
@@ -24,31 +24,11 @@ function App() {
     },
   ];
   return (
-    <div className="App">
-      <h2>Let's get Started</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-   
-   <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-   
-   <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem>
-   
+
+
+    <div>
+      <h2>Let's Get Stared</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
